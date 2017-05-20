1654ce2ebb5143715c5c40e17d24f96578586693
Jeffrey Chen <jeffreychen41@gmail.com>
2017-05-20 10:47:23 -0700

    Added STRIPE API

--- client/components/PendingEvent.jsx
+++ client/components/PendingEvent.jsx
@@ -6,6 +6,26 @@ class PendingEvent extends React.Component {
     this.state = {}
   }
 
+  acceptJob() {
+    this.props.accept(this.props.pending.name);
+    $.ajax({
+      type:'POST',
+      url: '/stripe',
+      contentType: 'application/JSON',
+      data: JSON.stringify({
+        accept: this.props.pending
+      })
+    })
+    .done((data) => {
+      console.log('receipt send via email');
+    })
+    .fail((err) => {
+      console.log('did not send receipt');
+    })
+
+  }
+
+
   render() {
     console.log('pending props', this.props.pending)
     return (
@@ -17,7 +37,7 @@ class PendingEvent extends React.Component {
             <p><strong>Date:</strong> {this.props.pending.date}</p>
             <p><strong>Time:</strong> {this.props.pending.start_time}</p>
             <p><strong>Description:</strong> {this.props.pending.description}</p>
-            <button className="btn btn-primary btn-sm" onClick={() => this.props.accept(this.props.pending.name)}>Accept</button>
+            <button className="btn btn-primary btn-sm" onClick={this.acceptJob.bind(this)}>Accept</button>
             <button className="btn btn-primary btn-sm" onClick={() => this.props.deny(this.props.pending.name)}>Deny</button>
           </div>
         </div>
