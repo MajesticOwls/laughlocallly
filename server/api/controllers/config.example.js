b95a958f4a6ecbea97dc03de12a009649c7cff3a
Jeffrey Chen <jeffreychen41@gmail.com>
2017-05-20 18:31:26 -0700

    EDITING TEXT FOR TWILIO

---
+++ server/api/controllers/config.example.js
@@ -0,0 +1,5 @@
+module.exports = {
+  twilio_ID: 'CLIENT_ID_GOES_HERE',
+  twilio_API_KEY: 'GOES_HERE',
+  STRIPE_ID: 'GOES_HERE'
+};
---
+++ server/api/controllers/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  TWILIO_ID: 'AC5ab320dfc2f2587e3cd2f8a3d2e7d412',
+  TWILIO_API_KEY: '51f14eada9d32808863dda6112f11b61',
+  STRIPE_ID: 'sk_test_np8ojqYV5pNwaLN2dtx7ubmt'
+};
