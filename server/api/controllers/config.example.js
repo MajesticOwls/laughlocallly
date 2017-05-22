43de2f5e31bf6a085f91ae038f1f577120bc33d0
Jeffrey <jeffreychen41@gmail.com>
2017-05-21 23:01:29 -0700

    Update config.example.js

--- server/api/controllers/config.example.js
+++ server/api/controllers/config.example.js
@@ -1,5 +1,5 @@
 module.exports = {
-  TWILIO_ID: 'AC5ab320dfc2f2587e3cd2f8a3d2e7d412',
-  TWILIO_API_KEY: '51f14eada9d32808863dda6112f11b61',
-  STRIPE_ID: 'sk_test_np8ojqYV5pNwaLN2dtx7ubmt'
+  TWILIO_ID: 'CLIENT_ID_GOES_HERE',
+  TWILIO_API_KEY: 'GOES_HERE',
+  STRIPE_ID: 'GOES_HERE'
 };
